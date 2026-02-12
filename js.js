6f410ed949f01ae1c1a1e0790e296a7f9d4d5568
lui-cloud <luizmlzc82@gmail.com>
2026-02-12 11:33:42 -0300

    Commit de login

--- js.js
+++ js.js
@@ -1,38 +1,28 @@
-let nomes = ["luiz", "joao", "nesk", "sorah", "polloh"]
+let nomes = ["luiz", "marcelo", "joao"]
 
-function verificarNome(nome) {
-    if (!nomes.includes(nome)) {
+function verificarNomes(nome) {
+    if(!nomes.includes(nome)) {
         return "acesso negado"
     }
 
-    if (nome === "luiz") {
+    if(nome === "luiz") {
         return "bem vindo adm"
-    } else if (nome === "nesk") {
-        return "bem vindo streammer"
     }
-
-    return "Bem-vindo, usuario"
-
+        return "acesso negado"   
 }
 
-// pegar elementos do html
-const input = document.querySelector("#nomeInput");
-const botao = document.querySelector("#btnBuscar");
-const resultadoEl = document.querySelector("#resultado");
+const input = document.querySelector("#userName");
+const botao = document.querySelector("#botao");
+const resultadoEl = document.querySelector("#result");
 
-
-// 4) Quando clicar no botão, pegar o nome e mostrar o resultado
-
-botao.addEventListener("click", () => {
+botao.addEventListener ("click", () => {
     const nomeDigitado = input.value.trim().toLowerCase();
-    const resposta = verificarNome(nomeDigitado);
+    const resposta = verificarNomes(nomeDigitado);
     resultadoEl.innerText = resposta;
 });
 
 input.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    const nomeDigitado = input.value.trim().toLowerCase();
-    const resposta = verificarNome(nomeDigitado);
-    resultadoEl.innerText = resposta;
-  }
+    if (e.key === "Enter") {
+        botao.click();
+    }
 });
